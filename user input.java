95809f52821cef990ccb26ffcb3cfe57490a12f2
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-04 16:56:00 +0530

    Create user input.java

---
+++ user input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+/*****getting input from user*****/
+public class input demo{
+   public static void main(string args[]){
+    Scanner scanner=new Scanner(system.in);
+    System.out.println("whatis your name?");
+    String name=Scanner.nextline();
+    System.out.println("hello" + name);
+ } 
+} 
