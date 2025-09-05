8866ebd72076c3f24a86db782c082c5f142d8033
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-05 16:52:07 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,44 @@
+//for loop
+
+public class for loop{
+    public static void main(string[]args){
+        for(int counter=1; counter<=5; counter++){
+            system.out.print("*");
+        }
+    }
+    }
+
+//while loop
+
+public class while loop{
+    public static void main(string[]args){
+        i=1;
+        while(i<=5){
+            system.out.print(i);
+            i++;
+        }
+    }
+
+//do while loop
+
+public class do while loop{
+    public static void main(string[]args){
+        do{
+            system.out.print(i);
+            i++;
+        }while(i<=5);
+    }
+}
+
+//nested loop
+
+public class nested loop{
+    public static void main(string[]args){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                system.out.print("*");
+            }
+            system.out.print("\n");
+        }
+    }
+}
