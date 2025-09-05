9d09076cf1d6c49bd38e767e4fbf10eb2d06b470
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-05 16:51:04 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,23 @@
+public class ArraysDemo{
+    public static void main(string[]args){
+        int marks[]=new int[6];
+        marks[0]=95;
+        marks[1]=76;
+        marks[2]=84;
+        system.out.println(marks[1]);
+    }
+}
+
+public class ArraysDemo{
+    public static void main(string[]args)[
+        float oneD[]=new float[50];
+        char[]=c1,c2,c3;
+        int[][]a=new int[3][4];
+        a[0][0]=85;
+        a[2][3]=78;
+        int b[][]=new int[4][];
+        b[0]=new int[5];
+        b[1]=new int[3];
+        system.out.println(a[2][3]);
+    ]
+    }
