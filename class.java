2976780a7aa1ed895c74aeaa224fff527480043e
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-06 17:20:51 +0530

    Create class.java

---
+++ class.java
@@ -0,0 +1,20 @@
+public class Demo class{
+    public static void main(string[]args){
+        class box{
+            int length;
+            int breadth;
+            int height;
+        }
+    public class boxes{
+        box blackbox;
+        blackbox=new box();
+
+        blackbox.length=10;
+        blackbox.breadth=8;
+
+        box woodbox=newbox();
+        woodbox.height=25;
+        system.out.println(blackbox.length);
+        system.out.println(woodbox.height);
+    }    
+    }
