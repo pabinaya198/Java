6670a410c3b153536b8856df349a0774fa409266
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-06 17:19:39 +0530

    Create array using loops.java

---
+++ array using loops.java
@@ -0,0 +1,33 @@
+//array using loops
+
+public class array using loops{
+    public static void main(string[]args){
+        int[] a={2,4,3,5,1,6};
+        for(int i=0;i<a.length;i++)
+        system.out.println(a[i]);
+    }
+}
+
+import java.util.Scanner;
+public class array using loops{
+    public static void main(string[]args){
+        int[] a=new int[6];
+        Scanner scanner=new Scanner(system.in);
+        system.out.println("enter 6 elements of the array:");
+        for(int i=0;i<a.length;i++)
+        a[i]=scanner.nextInt();
+    }
+    }
+
+//for each array
+
+public class array using loops{
+    public static void main(string[]args){
+        int arr[]={5,6,4,3,6,7};
+        for(int i=0;i<arr.length;i++)
+        system.out.println(arr[i]);
+        for(int j:arr){
+            system.out.println(j);
+        }
+        }
+    }
