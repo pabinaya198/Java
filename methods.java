7db975205578dfd456777d32cf44275caf8764e5
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-06 17:21:40 +0530

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,18 @@
+public class methods{
+    public static void main(string[]args){
+        static int boxvolume(int length,int breadth,int height){
+            int vol;
+            vol=length*breadth*height;
+            return vol;
+        }
+        static void printline(){
+            system.out.println("-----------------------------------");
+        }
+        public static void main(string[]args){
+            printline();
+            system.out.println("Hello");
+            int volume=box volume(10,4,6);
+            printline();
+        }
+    }
+    }
