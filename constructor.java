69adea8f7cd029cb92fe257e8ce790dc8be0390c
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-08 17:04:45 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,16 @@
+class box{
+    int length;
+    int breadth;
+    int height;
+}
+box(int l,int b,int h){
+    length=l;
+    breadth=b;
+    height=h;
+}
+void setdim(int l,int b,int h){
+    length=l;
+    breadth=b;
+    height=h;
+}
+blackbox.setdim(89,52,33);
