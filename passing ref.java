df5a6a6f5ef7ecc4881ab7aa1de8ad53dd6bd7b4
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-09 17:06:52 +0530

    Create passing ref.java

---
+++ passing ref.java
@@ -0,0 +1,11 @@
+Package OOP;
+public class passing ref{
+    static void increment(int a){
+        a++;
+    }
+    public static void main(string[]args){
+        int a=10;
+        increment(a);
+        system.out.println(a);
+    }
+}
