429c41d317696f0e82ca2bf3fe958b18cb25e543
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-13 16:48:29 +0530

    Create val arguments.java

---
+++ val arguments.java
@@ -0,0 +1,16 @@
+public class val arguments{
+    int min value(int.......vals){
+        static int min=Integer.MAX-value;
+        for(int k:vals){
+            if(k<min)
+            min=k;
+        }
+        return min;
+    }
+    public static void main(string[]args){
+        int m=min value(5,2,6);
+        system.out.println(m);
+        int n=min value(4,7,2,0,-1);
+        system.out.println(n);
+    }  
+}
