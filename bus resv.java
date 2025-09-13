d84912b6a4560243f9ac5491808a9686072593bb
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-09-13 16:50:14 +0530

    Create bus resv.java

---
+++ bus resv.java
@@ -0,0 +1,15 @@
+Package bus Resv;
+import java.util.Scanner;
+public class bus{
+    public static void main(string[]args){
+        int useropt=1;
+        Scanner scanner=new Scanner(system.in);
+        while(useropt==1){
+            system.out.println("enter 1 to book and 2 to exit");
+            useropt=Scanner.nextInt();
+            if(useropt==1){
+                system.out.println("booking........");
+            }
+        }
+    }
+}
